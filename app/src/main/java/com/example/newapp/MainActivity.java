b28feaf096686d1180ab869862e46a23f56adf6f
Sciforearth <67154128+deepanshubalyan01@users.noreply.github.com>
2024-06-03 17:32:12 +0530

    initial commit

--- app/src/main/java/com/example/newapp/MainActivity.java
+++ app/src/main/java/com/example/newapp/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         App app = new App("your app id");
 
 
