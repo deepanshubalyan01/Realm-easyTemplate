22f4bbd40fdfc47873331033aa46cd2181c71110
Sciforearth <67154128+deepanshubalyan01@users.noreply.github.com>
2024-06-03 17:26:57 +0530

    inital commit

---
+++ app/src/main/java/com/example/newapp/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.newapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import io.realm.mongodb.App;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        App app = new App("sdfds");
+
+
+    }
+}
